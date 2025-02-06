10fa726e1062388493011374306af629c20e8033
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-02-06 17:21:13 -0500

    "Selection Sort"

--- Arrays/BubbleSort.java
+++ Arrays/BubbleSort.java
@@ -16,11 +16,29 @@ public static void BubbleSort(int arr[]){
 }
 }
 
+public static void SelectionSort(int arr[]){
+    for(int i=0;i<=arr.length-1;i++){
+        int smallest=i;
+        for(int j=i+1;j<=arr.length-1;j++){
+            if(arr[smallest]>arr[j]){
+                smallest=j;
+            }
+        }
+
+        int temp=arr[smallest];
+        arr[smallest]=arr[i];
+        arr[i]=temp;
+    }
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+}
+
 
     public static void main(String args[]){
         int[] arr={4,5,3,1,2};
         BubbleSort(arr);
-      
+        SelectionSort(arr);
     }
     
 }
