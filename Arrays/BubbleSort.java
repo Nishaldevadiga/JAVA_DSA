1c0be3bf604a9c78eaf1cdbdbe493ae3c047055d
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-02-06 20:15:03 -0500

    "Counting sort"

--- Arrays/BubbleSort.java
+++ Arrays/BubbleSort.java
@@ -52,7 +52,33 @@ for(int i=1;i<arr.length-1;i++){
 for(int i=0;i<arr.length;i++){
     System.out.print(arr[i]);
 }
+}
 
+public static void CountingSort(int arr[]){
+    int largest=Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        largest=Math.max(largest,arr[i]);
+    }
+
+    //freq matrix
+    int[] freq=new int[largest+1];
+    for(int i=0;i<arr.length;i++){
+        freq[arr[i]]++;
+    }
+
+    //sorting 
+    int j=0;
+    for(int i=0;i<freq.length;i++){
+        while(freq[i]>0){
+            arr[j]=i;
+            j++;
+            freq[i]--;
+        }
+    }
+
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]);
+    }
 }
 
 
@@ -61,6 +87,7 @@ for(int i=0;i<arr.length;i++){
         BubbleSort(arr);
         SelectionSort(arr);
         InsertionSort(arr);
+        CountingSort(arr);
     }
     
 }
