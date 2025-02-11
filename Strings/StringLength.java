3d5de557bb2f3faa031073ced74a51b807fcb186
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-02-11 11:20:45 -0500

    "String length"

---
+++ Strings/StringLength.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str1="hello World";
+
+        System.out.println(str1.length());
+
+    }
+    
+}
