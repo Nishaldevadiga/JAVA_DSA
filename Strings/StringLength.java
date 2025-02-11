8511f61702a20ef880b5c3f771887b9f01ac4905
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-02-11 11:28:33 -0500

    "display str"

--- Strings/StringLength.java
+++ Strings/StringLength.java
@@ -1,10 +1,17 @@
 package Strings;
 
 public class StringLength {
+
+    public static void DisplayLetters(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
     public static void main(String[] args) {
         String str1="hello World";
 
         System.out.println(str1.length());
+        DisplayLetters(str1);
 
     }
     
