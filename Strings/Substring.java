71beb69e0ee0af1e6e0612b24faebc72fac8847b
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-02-11 12:33:08 -0500

    "Substring"

---
+++ Strings/Substring.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Substring {
+
+    public static void main(String[] args) {
+        String str1="HelloWorld";
+        System.out.println(str1.substring(0,5));
+    }
+    
+}
