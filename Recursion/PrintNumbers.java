3b1b8cb4521b1f49e8b2524cc67a4e41a2b61191
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-02-27 11:13:51 -0500

    "increasing"

--- Recursion/PrintNumbers.java
+++ Recursion/PrintNumbers.java
@@ -8,12 +8,20 @@ public static void Recursion(int n){
         }
         System.out.print(n+" ");
         Recursion(n-1);
-    
+}
 
+public static void printInc(int n){
+    if(n==1){
+        System.out.print(n+" ");
+        return;
+    }
+    printInc(n-1);
+    System.out.print(n+" ");
 }
     public static void main(String[] args) {
        int n=10;
        Recursion(n);
+       printInc(n);
     }
     
 }
