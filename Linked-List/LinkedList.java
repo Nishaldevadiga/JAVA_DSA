42f72185caf306f5cebbfb337c295b18cbb149c9
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-03-30 17:51:37 -0400

    "formar"

--- Linked-List/LinkedList.java
+++ Linked-List/LinkedList.java
@@ -1,145 +1,148 @@
 
-
 class LinkedList {
-    
-    public class Node{
+
+    public class Node {
         int data;
         Node next;
-        
-        public Node(int data){
-            this.data=data;
-            this.next=null;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
     public static int size;
-    
-    public  void addFirst(int data){
-        //create node
-        
-        Node newNode=new Node(data);
-        size++;
-        
-        if(head==null){
-            head=tail=newNode;
-            return;
-        }
-        
-        //assign next
-        
-        newNode.next=head;
-        
-        //assign head
-        head=newNode;
-    }
-    
-    public void addLast(int data){
-        
-        //create a node
-        Node newNode=new Node(data);
-        size++;
-        
-        if(head==null){
-            head=tail=newNode;
-            return;
-        }
-        
-        tail.next=newNode;
-        
-        tail=newNode;
-        
-    }
-    public void print(){
-        
-        Node temp=head;
-        while(temp!=null){
-        System.out.print(temp.data+" ");
-        temp=temp.next;
-        }
-    } 
 
-    public void removeFirst(){
-      
-        if(head==null){
+    public void addFirst(int data) {
+        // create node
+
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // assign next
+
+        newNode.next = head;
+
+        // assign head
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+
+        // create a node
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+
+        tail = newNode;
+
+    }
+
+    public void print() {
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public void removeFirst() {
+
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
-        head=head.next;
+        head = head.next;
         size--;
-       if(head==null){
-        tail=null;
-       }
-    }    
+        if (head == null) {
+            tail = null;
+        }
+    }
 
-    public void removeLast(){
-        if(size==0){
+    public void removeLast() {
+        if (size == 0) {
             System.out.println("List is empty");
             return;
-        }else if(size==1){
+        } else if (size == 1) {
 
-            head=tail=null;
+            head = tail = null;
             size--;
             return;
         }
-        Node temp=head;
+        Node temp = head;
 
-        for(int i=0;i<size-2;i++){
-temp=temp.next;
+        for (int i = 0; i < size - 2; i++) {
+            temp = temp.next;
         }
 
-        temp.next=null;
-        tail=temp;
+        temp.next = null;
+        tail = temp;
         size--;
     }
-    public void add(int idx, int data){
-    if(idx==0){
-       addFirst(data);
-        return;
-    }
-    
-    int i=0;
-    Node temp=head;
-    
-    while(i<idx-1){
-        temp=temp.next;
-        i++;
-    }
-    
-    Node newNode=new Node(data);
-    size++;
-    newNode.next=temp.next;
-    temp.next=newNode;
-    
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        int i = 0;
+        Node temp = head;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        Node newNode = new Node(data);
+        size++;
+        newNode.next = temp.next;
+        temp.next = newNode;
+
     }
 
-    public int Search(int key){
-          
-        Node temp=head;
-        int i=0;
+    public int Search(int key) {
 
-        while(temp!=null){
-            if(temp.data==key){
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
                 return i;
             }
-            temp=temp.next;
+            temp = temp.next;
             i++;
         }
         return -1;
     }
+
     public static void main(String[] args) {
-        LinkedList ll=new LinkedList();
-        
+        LinkedList ll = new LinkedList();
+
         ll.addFirst(1);
-          ll.addLast(2);
-          ll.add(1,7);
-          ll.print();
-          System.out.println();
-        //ll.removeFirst();
+        ll.addLast(2);
+        ll.add(1, 7);
+        ll.print();
+        System.out.println();
+        // ll.removeFirst();
         ll.removeLast();
         ll.print();
         System.out.println();
-        System.out.println("Size= "+ll.size);
+        System.out.println("Size= " + ll.size);
         System.out.println(ll.Search(2));
     }
 }
