13596acaaba481a32254b43476eb8fbd55d12a7d
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-03-30 17:43:17 -0400

    "removelast"

--- Linked-List/LinkedList.java
+++ Linked-List/LinkedList.java
@@ -58,7 +58,40 @@ class LinkedList {
         temp=temp.next;
         }
     } 
-    
+
+    public void removeFirst(){
+      
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        head=head.next;
+        size--;
+       if(head==null){
+        tail=null;
+       }
+    }    
+
+    public void removeLast(){
+        if(size==0){
+            System.out.println("List is empty");
+            return;
+        }else if(size==1){
+
+            head=tail=null;
+            size--;
+            return;
+        }
+        Node temp=head;
+
+        for(int i=0;i<size-2;i++){
+temp=temp.next;
+        }
+
+        temp.next=null;
+        tail=temp;
+        size--;
+    }
     public void add(int idx, int data){
     if(idx==0){
        addFirst(data);
@@ -85,6 +118,10 @@ class LinkedList {
         ll.addFirst(1);
           ll.addLast(2);
           ll.add(1,7);
+          ll.print();
+          System.out.println();
+        //ll.removeFirst();
+        ll.removeLast();
         ll.print();
         System.out.println();
         System.out.println("Size= "+ll.size);
