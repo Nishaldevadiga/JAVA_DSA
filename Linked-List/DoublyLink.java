d6bb4dea8737543972e5bea3854a209426bb9a34
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-01 19:31:27 -0400

    "doublyll"

--- Linked-List/DoublyLink.java
+++ Linked-List/DoublyLink.java
@@ -13,14 +13,51 @@ public class DoublyLink {
              this.prev=null;
         }
 
-        public static Node head;
-        public static Node tail;
-        public static int size;
 
     }
 
+    public static Node head;
+    public static Node tail;
+    public static int size=0;
+
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+
+        if(head==null){
+            size++;
+            head=tail=newNode;
+            return;
+         
+        }
+    
+        newNode.next=head;
+        head.prev=newNode;
+        head=newNode;
+         size++;
+    }
+
+    public void print(){
+        Node temp=head;
+
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        DoublyLink ll=new DoublyLink();
+
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+
+
+        ll.print();
+        System.err.println("size= "+size);
+
+
+    
 
 
     }
