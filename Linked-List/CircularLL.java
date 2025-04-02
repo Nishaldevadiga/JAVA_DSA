33a611000646fe95ce4d90fdf347bc27bd07449c
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-02 12:14:31 -0400

    "format"

--- Linked-List/CircularLL.java
+++ Linked-List/CircularLL.java
@@ -1,53 +1,53 @@
 public class CircularLL {
 
-    class Node{
+    class Node {
 
         int data;
         Node next;
 
-        public Node(int data){
-            this.data=data;
+        public Node(int data) {
+            this.data = data;
         }
     }
+
     public static Node head;
     public static Node tail;
-    public static int size=0;
+    public static int size = 0;
 
-    public void addFirst(int data){
+    public void addFirst(int data) {
 
-        Node newNode=new Node(data);
+        Node newNode = new Node(data);
 
-        if(head==null){
-            head=tail=newNode;
-            tail.next=head;
+        if (head == null) {
+            head = tail = newNode;
+            tail.next = head;
             size++;
             return;
         }
 
-            newNode.next=head;
-            head=newNode;
-            tail.next=newNode;
-            size++;
-
+        newNode.next = head;
+        head = newNode;
+        tail.next = newNode;
+        size++;
 
     }
-    public static void print(){
-        Node temp=head;
 
-        do{
-            System.out.print(temp.data+"->");
-            temp=temp.next;
-        }while(temp!=head);
+    public static void print() {
+        Node temp = head;
+
+        do {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        } while (temp != head);
         System.out.println("(Head)");
     }
 
     public static void main(String[] args) {
-        CircularLL ll=new CircularLL();
+        CircularLL ll = new CircularLL();
 
         ll.addFirst(2);
         ll.addFirst(1);
-      ll.print();
-
+        ll.print();
 
     }
 }
