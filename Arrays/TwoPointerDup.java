1fb3bc047d1d1516b2ddfc932fe3cc8eae8262a2
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-07 16:13:11 -0400

    "remove duplicates"

---
+++ Arrays/TwoPointerDup.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class TwoPointerDup {
+public static int RemoveDup(int arr[]){
+
+    int i=0;
+    for(int j=1;j<arr.length;j++){
+        if(arr[i]!=arr[j]){
+            i++;
+            arr[i]=arr[j];
+        }
+    }
+    return i+1;
+}
+    public static void main(String args[]){
+        int[] arr={1,1,1,2,2,3};
+        System.out.println(RemoveDup(arr));
+    }
+    
+}
