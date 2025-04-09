506a63b0c304a7a4f3b4ad5efae76643e7f0011e
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-09 17:40:16 -0400

    "sliding window"

--- Sliding_Window/Maxpointsliding.java
+++ Sliding_Window/Maxpointsliding.java
@@ -1,7 +1,36 @@
 package Sliding_Window;
 
 public class Maxpointsliding {
-    public static void main(String args[]){
-        
+
+    public static int Maxpoints(int arr[], int k) {
+        int l = 0;
+        int r = k - 1;
+
+        int lsum = 0;
+        int rsum = 0;
+        int rightidx = arr.length - 1;
+        int Maxsum = 0;
+
+        for (int i = 0; i <= r; i++) {
+            lsum += arr[i];
+        }
+
+        while (r>0) {
+            lsum = lsum - arr[r];
+            r--;
+            rsum = rsum + arr[rightidx];
+            rightidx--;
+            Maxsum = Math.max(Maxsum, lsum + rsum);
+        }
+        return Maxsum;
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = { 6, 2, 3, 4, 7, 2, 1, 7, 1 };
+
+        int result=Maxpoints(arr, 4);
+        System.out.println(result);
+
     }
 }
