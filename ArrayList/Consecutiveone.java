1e3bddcd3f9468c08eeacf5e8163557095d24fa8
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-10 18:03:05 -0400

    "customized soln"

--- ArrayList/Consecutiveone.java
+++ ArrayList/Consecutiveone.java
@@ -25,6 +25,29 @@ public class Consecutiveone {
         return maxlen;
     }
 
+    public static int ConsecutiveCus(ArrayList<Integer> list, int k){
+        int l=0,r=0,maxlen=0,zero=0;
+       
+
+        while(r<list.size()){
+            if(list.get(r)==0){
+                zero++;
+            }
+           if(zero<=k){
+            maxlen=Math.max(maxlen, (r-l)+1);
+           }
+           if(zero>k){
+            if(list.get(l)==0){
+                zero--;
+            }
+            l++;
+           }
+           r++;
+        }
+
+        return maxlen;
+    }
+
     public static void main(String args[]) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
@@ -33,12 +56,20 @@ public class Consecutiveone {
         list.add(0);
         list.add(0);
         list.add(1);
+        list.add(0);
         list.add(1);
+        list.add(0);
         list.add(1);
+        list.add(0);
+        list.add(0);
+        list.add(0);
         list.add(1);
+        list.add(0);
         list.add(0);
         int result = Consecutive(list, 2);
         System.out.println(result);
+        int result1=ConsecutiveCus(list, 2);
+        System.out.println(result1);
     }
 
 }
