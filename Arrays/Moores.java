e7f82279331a22784b4b9bde743bba16387696aa
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-18 12:56:50 -0400

    "Moores Alogo"

--- Arrays/Moores.java
+++ Arrays/Moores.java
@@ -18,11 +18,39 @@ public class Moores {
 
     }
 
+    public static int Moores(int arr[]) {
+        int count = 0;
+        int ele = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (count == 0) {
+                count = 1;
+                ele = arr[i];
+            } else if (arr[i] == ele) {
+                count++;
+            } else {
+                count--;
+            }
+
+        }
+        count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ele) {
+                count++;
+            }
+        }
+        if (count > arr.length / 2) {
+            return ele;
+        }
+        return -1;
+
+    }
+
     public static void main(String[] args) {
 
         int arr[] = { 2, 3, 3, 3, 1, 3, 2 };
 
-        System.out.println(Max(arr));
+        // System.out.println(Max(arr));
+        System.out.println(Moores(arr));
 
     }
 
