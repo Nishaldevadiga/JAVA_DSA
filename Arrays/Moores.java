48569d955a20b65a6de39b62b92fdd94548c0a68
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-18 12:44:14 -0400

    "formatted"

--- Arrays/Moores.java
+++ Arrays/Moores.java
@@ -3,28 +3,27 @@ package Arrays;
 import java.util.HashMap;
 
 public class Moores {
+    // O(n) TC and 0(n) SC
+    public static int Max(int arr[]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-    public static int Max(int arr[]){
-        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
 
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0 )+1);
-
-            if(map.get(arr[i])>arr.length/2){
+            if (map.get(arr[i]) > arr.length / 2) {
                 return arr[i];
             }
         }
-return -1;
-       
+        return -1;
+
     }
 
     public static void main(String[] args) {
 
-        int arr[]={2,3,3,3,1,3,2};
+        int arr[] = { 2, 3, 3, 3, 1, 3, 2 };
 
         System.out.println(Max(arr));
 
-        
     }
-    
+
 }
