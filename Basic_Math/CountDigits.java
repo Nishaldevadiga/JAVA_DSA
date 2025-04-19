e3ae519857055a9a8027d6f611aa1683e814b8d8
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-19 17:04:48 -0400

    "reverse"

--- Basic_Math/CountDigits.java
+++ Basic_Math/CountDigits.java
@@ -10,8 +10,21 @@ public class CountDigits {
 
     }
 
+    public static int reverseDigits(int num){
+        int revnum=0,lastdigit=0,n=0;
+
+        while(num>0){
+            lastdigit=num%10;
+             num=num/10;
+             revnum=revnum*10+lastdigit;
+        }
+
+        return revnum;
+    }
+
     public static void main(String[] args) {
-        System.out.print(count(77568));
+        //System.out.print(count(77568));
+        System.out.print(reverseDigits(4567));
     }
     
 }
