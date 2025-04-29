129db00ae142359548b7f6d108e72746313f3679
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-29 18:02:19 -0400

    "using collection framework"

---
+++ Stacks/StackCollection.java
@@ -0,0 +1,22 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class StackCollection {
+
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        
+
+        while (!s.isEmpty()) {
+            System.out.print(s.peek());
+            s.pop();
+        }
+    }
+    
+}
