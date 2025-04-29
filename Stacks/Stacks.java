a5926bfeda906d784c1cecc8860bda3dc6435542
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-29 17:42:50 -0400

    "changes"

--- Stacks/Stacks.java
+++ Stacks/Stacks.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Stacks {
 
     static class stack {
-       static ArrayList<Integer> list = new ArrayList<>();
+        static ArrayList<Integer> list = new ArrayList<>();
 
         public static boolean isEmpty() {
             return list.size() == 0;
@@ -16,12 +16,18 @@ public class Stacks {
         }
 
         public static int pop() {
+            if(list.size()==0){
+                return -1;
+            }
             int ele = list.get(list.size() - 1);
             list.remove(list.size() - 1);
             return ele;
         }
 
         public static int peek() {
+            if(list.size()==0){
+                return -1;
+            }
             return list.get(list.size() - 1);
         }
     }
---
+++ Stacks/StacksLinked.java
@@ -0,0 +1,9 @@
+package Stacks;
+
+public class StacksLinked {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
