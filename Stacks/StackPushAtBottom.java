f42db47a5fbba9338fee4f5616851ed64ddd3a98
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-04-30 17:07:40 -0400

    "reverse"

--- Stacks/StackPushAtBottom.java
+++ Stacks/StackPushAtBottom.java
@@ -4,32 +4,42 @@ import java.util.Stack;
 
 public class StackPushAtBottom {
 
-    public static void PushBottom(Stack<Integer>  s, int data){
-         if(s.isEmpty()){
+    public static void PushBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
             s.push(data);
             return;
-         }
+        }
 
-        int top=s.pop();
+        int top = s.pop();
         PushBottom(s, data);
         s.push(top);
 
     }
 
+    public static void ReverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        ReverseStack(s);
+        PushBottom(s, top);
+    }
+
     public static void main(String[] args) {
-        Stack<Integer> s=new Stack<>();
+        Stack<Integer> s = new Stack<>();
 
         s.push(1);
         s.push(2);
         s.push(3);
 
-        PushBottom(s,5);
+        PushBottom(s, 5);
+        ReverseStack(s);
 
-        while(!s.isEmpty()){
+        while (!s.isEmpty()) {
             System.out.println(s.pop());
         }
 
-        
     }
-    
+
 }
