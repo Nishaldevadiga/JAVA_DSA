c53cc8c000811545a9e7708fa26be807d44e91ab
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-05-02 18:25:31 -0400

    "optimized circular queue"

--- Queue/QueueB.java
+++ Queue/QueueB.java
@@ -1,62 +1,78 @@
 package Queue;
 
 public class QueueB {
-static class Queue{
-     int arr[];
-    int size;
-    int rear;
+    static class Queue {
+        int arr[];
+        int size;
+        int rear;
+        int front;
 
-    Queue(int n){
-        arr=new int[n];
-        size=n;
-        rear=-1;
-    }
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
 
-    public boolean isEmpty(){
-        return rear==-1;
-    }
-
-    public int remove(){
-        if(isEmpty()){
-            return -1;
         }
-       int front=arr[0];
 
-       for(int i=0;i<rear;i++){
-        arr[i]=arr[i+1];
-       }
-       rear=rear-1;
-       return front;
-    }
-
-    public void add(int data){
-        if(rear==arr.length-1){
-           System.out.println("Queue is full");
-           return;
+        public boolean isEmpty() {
+            return rear == -1 && front == -1;
         }
-       rear=rear+1;
-       arr[rear]=data;
-    }
-    public int peek(){
-        if(isEmpty()){
-            return -1;
+
+        public boolean isfull() {
+            return (rear + 1) % size == front;
         }
-        int top=arr[0];
-        return top;
+
+        public int remove() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int result = arr[front];
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        public void add(int data) {
+            if (isfull()) {
+                System.out.println("Queue is full");
+                return;
+            }
+            // adding element for the first time
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = arr[front];
+            return top;
+        }
+
     }
 
-
-}
     public static void main(String[] args) {
-        Queue q=new Queue(3);
+        Queue q = new Queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
 
+        System.out.println(q.remove());
+
         while(!q.isEmpty()){
-            System.out.println(q.peek()+" ");
+            System.out.print(q.peek()+" ");
             q.remove();
         }
+        
+
     }
-    
+
 }
