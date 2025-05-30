9df23e83da96dd5c08bd7ac2a2c15a62990224c1
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-05-30 19:00:49 -0400

    "optimized"

--- Stackss/Duplicatepara.java
+++ Stackss/Duplicatepara.java
@@ -3,22 +3,30 @@ package Stackss;
 import java.util.Stack;
 
 public class Duplicatepara {
-public static boolean isValid(String str){
-    Stack<Character> s=new Stack<>();
-    for(int i=0;i<str.length();i++){
-        if(str.charAt(i)==')'){
-         
-        }else{
-            
+    public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ')') {
+                int count = 0;
+                while (!s.isEmpty() && !(s.peek() == '(')) {
+                    s.pop();
+                    count++;
+                }
+                if (count == 0) {
+                    return true;
+                }
+                s.pop();
+            } else {
+                s.push(str.charAt(i));
+            }
+          
         }
+        return false;
     }
-return false;
-}
 
     public static void main(String[] args) {
-        String str="((a+b)+(c+d))";
-
-        
+        String str = "((a+b)+(c+d))";
+        System.out.println(isValid(str));
     }
-    
+
 }
