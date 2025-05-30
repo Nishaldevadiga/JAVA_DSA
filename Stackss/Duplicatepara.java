3be0a6dbbad541fc2cb1449f7c6123c7c41e10c8
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-05-30 18:45:39 -0400

    "duppara"

---
+++ Stackss/Duplicatepara.java
@@ -0,0 +1,24 @@
+package Stackss;
+
+import java.util.Stack;
+
+public class Duplicatepara {
+public static boolean isValid(String str){
+    Stack<Character> s=new Stack<>();
+    for(int i=0;i<str.length();i++){
+        if(str.charAt(i)==')'){
+         
+        }else{
+            
+        }
+    }
+return false;
+}
+
+    public static void main(String[] args) {
+        String str="((a+b)+(c+d))";
+
+        
+    }
+    
+}
