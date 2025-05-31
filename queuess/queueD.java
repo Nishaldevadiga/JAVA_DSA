78c1238f1cdb64b7c73bd03ec19f93b87b91394a
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-05-31 17:12:29 -0400

    "stack using 2 queue"

---
+++ queuess/queueD.java
@@ -0,0 +1,23 @@
+package queuess;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queueD {
+
+    public static void createStack(int arr[],Queue<Integer> q1,Queue<Integer> q2){
+           
+        
+    }
+
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3};
+        Queue<Integer> q1=new LinkedList<>();
+
+        Queue<Integer> q2=new LinkedList<>();
+
+        createStack(arr,q1,q2);
+    }
+    
+}
