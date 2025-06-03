91e3f16f6c433ec786688adcc06db06195fb0440
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-03 15:31:06 -0400

    "no of activity"

--- GreedyAlogs/Activities.java
+++ GreedyAlogs/Activities.java
@@ -2,8 +2,17 @@ package GreedyAlogs;
 
 public class Activities {
 
-    public static void main(String[] args) {
+    public static int NumberActivity(int start[], int end[]) {
         
+        return 0;
     }
-    
+
+    public static void main(String[] args) {
+
+        int start[] = { 10, 12, 20 };
+        int end[] = { 20, 25, 30 };
+
+        int result = NumberActivity(start, end);
+    }
+
 }
