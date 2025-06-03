859bb2b91241915fa1d652808314d0ccd4307678
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-03 15:39:24 -0400

    "max activities"

--- GreedyAlogs/Activities.java
+++ GreedyAlogs/Activities.java
@@ -1,18 +1,36 @@
 package GreedyAlogs;
 
+import java.util.ArrayList;
+
 public class Activities {
 
     public static int NumberActivity(int start[], int end[]) {
-        
-        return 0;
+
+        ArrayList<Integer> list=new ArrayList<>();
+        int count=1;
+        int starts=start[0];
+        int ends=end[0];
+        list.add(0);
+        for(int i=1;i<start.length;i++){
+            if(start[i]>=ends){
+                count++;
+                list.add(i);
+                ends=end[i];
+            }
+        }
+        for(int i=0;i<list.size();i++){
+            System.out.println("A"+list.get(i));
+        }
+        return count;
     }
 
     public static void main(String[] args) {
 
-        int start[] = { 10, 12, 20 };
-        int end[] = { 20, 25, 30 };
+        int start[] = { 1,3,0,5,8,5 };
+        int end[] = { 2,4,6,7,9,9};
 
         int result = NumberActivity(start, end);
+        System.out.print("Maximum Activities = "+result);
     }
 
 }
