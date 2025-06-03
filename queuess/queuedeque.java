e5a8ea30becd37e3fef7bdbe733b7f4503fd6d58
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-02 21:21:05 -0400

    "commit"

--- queuess/queuedeque.java
+++ queuess/queuedeque.java
@@ -5,35 +5,36 @@ import java.util.LinkedList;
 
 public class queuedeque {
 
-    static class queue{
-        static Deque<Integer> q=new LinkedList<>();
+    static class queue {
+        static Deque<Integer> q = new LinkedList<>();
 
-
-        public static boolean isEmpty(){
-            return q.size()==0;
+        public static boolean isEmpty() {
+            return q.size() == 0;
         }
 
-        public static void  add(int data){
+        public static void add(int data) {
             q.addLast(data);
         }
-        public static void remove(){
+
+        public static void remove() {
             q.removeFirst();
         }
-        public static int peek(){
+
+        public static int peek() {
             return q.getFirst();
         }
     }
 
     public static void main(String[] args) {
-        queue q=new queue();
+        queue q = new queue();
 
         q.add(1);
         q.add(2);
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
         }
     }
-    
+
 }
