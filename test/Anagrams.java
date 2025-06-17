f55e935f98af2e43bc338a0ab7a746497f950310
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-17 12:11:11 -0400

    "Anagram"

--- test/Anagrams.java
+++ test/Anagrams.java
@@ -1,18 +1,36 @@
 package test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Anagrams {
-    public static boolean isAnagram(String str1,String str2){
-        if(str1.length()!=str2.length()){
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) {
             return false;
         }
-        
-        return false;
+
+        char[] char1 = str1.toCharArray();
+        char[] char2 = str2.toCharArray();
+
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+
+        for (int i = 0; i < char1.length; i++) {
+
+            if (char1[i] != char2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        
+
+        String str1 = "nishal";
+        String str2 = "alhsin";
+
+        System.out.print(isAnagram(str1, str2));
+
     }
 
-    
 }
