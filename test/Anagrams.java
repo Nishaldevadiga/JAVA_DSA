a6b80518eb68f7761de6e9d1e3ecd74ce6517e4f
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-17 10:59:48 -0400

    "Anagrams"

---
+++ test/Anagrams.java
@@ -0,0 +1,18 @@
+package test;
+
+
+public class Anagrams {
+    public static boolean isAnagram(String str1,String str2){
+        if(str1.length()!=str2.length()){
+            return false;
+        }
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
