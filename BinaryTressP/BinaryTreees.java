ac78c21e8dcab62bfcd55480ce7dfd5dc4ded3f8
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-18 15:45:14 -0400

    "inorder"

--- BinaryTressP/BinaryTreees.java
+++ BinaryTressP/BinaryTreees.java
@@ -39,14 +39,26 @@ public class BinaryTreees {
             PreOrder(root.right);
 
         }
+
+        public static void Inorder(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+
+            Inorder(root.left);
+            System.out.print(root.data + " ");
+            Inorder(root.right);
+        }
     }
 
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.BuildTree(nodes);
-        //System.out.print(root.data);
-         tree.PreOrder(root);
+        // System.out.print(root.data);
+        // tree.PreOrder(root);
+        tree.Inorder(root);
 
     }
 }
