65ad401901efe293e2a1d3ee2b7c708fbe29f372
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-18 15:29:37 -0400

    "Preorder traversal"

--- BinaryTressP/BinaryTreees.java
+++ BinaryTressP/BinaryTreees.java
@@ -19,22 +19,34 @@ public class BinaryTreees {
 
         public static Node BuildTree(int[] nodes) {
             idx++;
-            if(nodes[idx]== -1){
+            if (nodes[idx] == -1) {
                 return null;
             }
-            Node newNode=new Node(nodes[idx]);
-            newNode.left=BuildTree(nodes);
-            newNode.right=BuildTree(nodes);
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
 
             return newNode;
+        }
+
+        public static void PreOrder(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            PreOrder(root.left);
+            PreOrder(root.right);
+
         }
     }
 
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        BinaryTree tree=new BinaryTree();
-        Node root=tree.BuildTree(nodes);
-        System.out.print(root.data);
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.BuildTree(nodes);
+        //System.out.print(root.data);
+         tree.PreOrder(root);
 
     }
 }
