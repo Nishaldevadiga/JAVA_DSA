ff9467d53648d07ac98a05ec279765ed65232c2e
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-07-02 13:19:47 -0400

    "optimized approach"

--- BinaryTreestwo/BinaryTreestwo.java
+++ BinaryTreestwo/BinaryTreestwo.java
@@ -52,15 +52,45 @@ public class BinaryTreestwo {
 
             return Math.max(selfdia,Math.max(rightdia, selfdia));
           }
+
+          static class info{
+            int ht;
+            int dia;
+
+            info(int ht,int dia){
+                this.ht=ht;
+                this.dia=dia;
+            }
+          }
+
+          public static info diameteropt(Node root){
+            if(root==null){
+               return new info(0,0);
+            }
+
+            info leftinfo=diameteropt(root.leftNode);
+            info rightinfo=diameteropt(root.rightNode);
+
+            int dia=Math.max(Math.max(leftinfo.dia,rightinfo.dia),leftinfo.ht+rightinfo.ht+1);
+            int ht=Math.max(leftinfo.ht, rightinfo.ht)+1;
+            
+
+            return new info(ht, dia);
+
+
+          }
     }
 
     public static void main(String args[]){
             int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
             BinaryTree tree=new BinaryTree();
             Node root=tree.BuildTree(nodes);
-            System.out.println(root.data);
-            System.out.println(tree.height(root));
-            System.out.println(tree.diameter(root));
+            //System.out.println(root.data);
+            //System.out.println(tree.height(root));
+            //System.out.println(tree.diameter(root));
+
+            System.out.println(tree.diameteropt(root).dia);
+
             
            
     }
