4aa0d614cb188ecb3166b1fa99169683b2cf35a0
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-07-02 12:24:58 -0400

    "Binary trees part two"

---
+++ BinaryTreestwo/BinaryTreestwo.java
@@ -0,0 +1,21 @@
+package BinaryTreestwo;
+
+public class BinaryTreestwo {
+
+    static class Node {
+         int data;
+         Node rightNode;
+         Node leftNode;
+
+         Node(int data){
+            this.data=data;
+            this.rightNode=null;
+            this.leftNode=null;
+         }
+    }
+
+    static class BinaryTree{
+        
+    }
+    
+}
