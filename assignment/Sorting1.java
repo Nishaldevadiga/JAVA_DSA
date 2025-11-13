4890b50c6c5ca84a7e84f6f24bde686f43fd5008
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-11-13 09:35:04 -0500

    "Bubble sorting"

--- assignment/Sorting1.java
+++ assignment/Sorting1.java
@@ -2,17 +2,46 @@ package assignment;
 
 public class Sorting1 {
 
-    public static void SelectionSort(int arr[]){
+    public static void SelectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
-        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
+    }
+
+    public static void BubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
     public static void main(String[] args) {
 
-        int arr[]={34,56,12,9};
-        SelectionSort(arr);
-        
+        int arr[] = { 34, 56, 12, 9 };
+        BubbleSort(arr);
+
     }
-    
+
 }
