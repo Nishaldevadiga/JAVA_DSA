deb018c683a70f1396f0928b62b41b250e2d4361
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-11-13 11:28:08 -0500

    "changes"

---
+++ assignment/StringMatching.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class StringMatching {
+
+    public static void StringMatchAlgo(String text, String pattern) {
+
+    }
+
+    public static void main(String[] args) {
+        String text = "HappyBirthday";
+        String pattern = "Birthday";
+    }
+
+}
