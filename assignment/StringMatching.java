5405dade8b1e46ead95d04994a39f68373913aac
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-11-13 11:47:10 -0500

    "string matching"

--- assignment/StringMatching.java
+++ assignment/StringMatching.java
@@ -9,6 +9,22 @@ public class StringMatching {
     public static void main(String[] args) {
         String text = "HappyBirthday";
         String pattern = "Birthday";
+
+        int n = text.length();
+        int m = pattern.length();
+
+        for (int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+            while (j < m && text.charAt(i + j) == pattern.charAt(j)) {
+                j++;
+            }
+
+            if (j == m) {
+                System.out.print("found " + i);
+                break;
+            }
+        }
     }
 
 }
