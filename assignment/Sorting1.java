d9977309f3642cca2f139ee8fa742ed6cddc0e51
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-11-13 10:14:37 -0500

    "Insertion sort"

--- assignment/Sorting1.java
+++ assignment/Sorting1.java
@@ -1,5 +1,7 @@
 package assignment;
 
+import Sliding_Window.longestsubstring;
+
 public class Sorting1 {
 
     public static void SelectionSort(int arr[]) {
@@ -37,10 +39,26 @@ public class Sorting1 {
         }
     }
 
+    public static void Insertion(int arr[]){
+        for(int i=0;i<=arr.length-1;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+
+        }
+             for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
     public static void main(String[] args) {
 
         int arr[] = { 34, 56, 12, 9 };
-        BubbleSort(arr);
+        Insertion(arr);
 
     }
 
